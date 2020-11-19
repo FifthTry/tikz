a79e08f0a9ecd56a6dbfe145f4598ff9af1c2b97
hitul007 <mistryhitul007@gmail.com>
2020-11-19 11:13:46 +0530

    Added tikz dependency file

---
+++ tikz-install.bash
@@ -0,0 +1,4 @@
+sudo apt-get install texlive-latex-base
+# sudo apt-get install -y cmake kdelibs5-dev libqt4-dev libpoppler-qt4-dev
+sudo apt-get install -y texlive-latex-extra texlive-science texlive-latex-base texlive-latex-recommended texlive-pstricks texlive-base-bin texlive-extra-utils preview-latex-style m4 ghostscript pdf2svg 
+
