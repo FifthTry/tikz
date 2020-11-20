4e97c2b95236bb1a7235e488ca5da4589307d892
ankitgoswami23 <ankitgoswami23@outlook.com>
2020-11-20 12:03:50 +0530

    Change in run_command functions

--- command/utils.py
+++ command/utils.py
@@ -1,7 +1,24 @@
-import subprocess
+from subprocess import Popen, PIPE, TimeoutExpired
 
 
-def run_command(cmd, timeout=20):
-    p = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    (output, err) = p.communicate(timeout=timeout)
-    return output, err
+# def run_command(cmd, timeout=20):
+#     p = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#     (output, err) = p.communicate(timeout=timeout)
+#     return output, err
+
+def run_command(cmd, timeout=5):
+
+    try:
+        with Popen(cmd.split(), stdout=PIPE, stderr=PIPE) as ps_process:
+            success, _ = ps_process.communicate(timeout=timeout)
+
+    except TimeoutExpired as e:
+        print("------" + str(e))
+        ps_process.kill()
+        success = "Timeout"
+    except Exception as e:
+        print("------" + str(e))
+        ps_process.kill()
+        success = "Exception"
+
+    return success
